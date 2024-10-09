8e21cb0823d27a83df86de8092982132a4fd9435
petereycoder <fp.pipe.reyes@gmail.com>
2024-10-09 15:59:50 -0600

    Se agregó sweetalert2 para los mensaje de crear y actualizar

--- resources/views/Games/index.blade.php
+++ resources/views/Games/index.blade.php
@@ -4,13 +4,13 @@
 @endsection
 @section('body')
     @if ($msj = Session::get('success'))
-        <div class="row">
-            <div class="col-md-4 ofsset-md-4">
-                <div class="alert alert-success">
-                    <p><i class="fa-solid fa-check"></i>{{$msj}}</p>
-                </div>
-            </div>
-        </div>
+    <script>
+        Swal.fire({
+            title: "Excelente",
+            text: "{{ $msj }}",
+            icon: "success"
+        });
+    </script>
     @endif
     <div class="row">
         <div class="col-12">
@@ -33,7 +33,7 @@
                                 <td>{{ $game->levels }}</td>
                                 <td>{{ $game->release }}</td>
                                 <td>
-                                    <img class="img-fluid" src="/storage/{{ $game->imagen }}" alt="Imagen">
+                                    <img class="img-fluid" src="storage\app\private\public{{ $game->image }}" alt="Imagen">
                                 </td>
                                 <td>
                                     <a class="btn btn-warning" href="{{route('games.edit', $game->id)}}">
@@ -60,3 +60,4 @@
         </div>
     </div>
 @endsection
+
